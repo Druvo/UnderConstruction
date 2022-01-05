2cd4cc5bc5184a4d1399f7828c252930518c0dfd
Zahid Hasan <zhdruvo@gmail.com>
2022-01-05 17:13:54 +0600

    Initial commit

---
+++ UnderConstruction/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnderConstruction.Startup))]
+namespace UnderConstruction
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
